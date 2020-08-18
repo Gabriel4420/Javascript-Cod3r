e0538b43861de9c6b481bab3363739b27aeaa897
= <gabriel_rodrigues_perez@hotmail.com>
2020-08-18 14:53:59 -0300

    alterações

---
+++ Projetos/Exercicios - JS/OO/OO.js
@@ -0,0 +1,19 @@
+/* Conceitos da OO */
+
+// Código não executável
+// procedural
+processamento(v1,v2,v3)
+
+
+// Orientação a Objetos
+
+objecto = {
+  value, 
+  value2, 
+  value3,
+  processamento(){
+    //...
+  }
+}
+
+objecto.processamento()
